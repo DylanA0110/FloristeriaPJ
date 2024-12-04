55b4830da0c91045667203f41991a363275ffaf1
Eduardo Antonio Hernandez Manzanares <eduardo.hernandez84u@std.uni.edu.ni>
2024-12-04 15:44:05 -0600

    sincronizar la rama a mi porra

--- UIFloristeria/Factura.Designer.cs
+++ UIFloristeria/Factura.Designer.cs
@@ -31,15 +31,7 @@
             DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
             DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
-            TxtPrimerNombre = new TextBox();
-            label1 = new Label();
             label2 = new Label();
-            label3 = new Label();
-            TxtSegundoNombre = new TextBox();
-            label4 = new Label();
-            TxtSegundoApellido = new TextBox();
-            label5 = new Label();
-            TxtPrimerApellido = new TextBox();
             label6 = new Label();
             TxtMontoTotal = new TextBox();
             label7 = new Label();
@@ -52,28 +44,6 @@
             ((System.ComponentModel.ISupportInitialize)DtgFacturas).BeginInit();
             SuspendLayout();
             // 
-            // TxtPrimerNombre
-            // 
-            TxtPrimerNombre.BackColor = Color.FromArgb(22, 58, 55);
-            TxtPrimerNombre.BorderStyle = BorderStyle.None;
-            TxtPrimerNombre.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
-            TxtPrimerNombre.Location = new Point(76, 121);
-            TxtPrimerNombre.Name = "TxtPrimerNombre";
-            TxtPrimerNombre.Size = new Size(140, 20);
-            TxtPrimerNombre.TabIndex = 0;
-            TxtPrimerNombre.Text = "_________________________________________________________________________";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.ForeColor = Color.White;
-            label1.Location = new Point(76, 84);
-            label1.Name = "label1";
-            label1.Size = new Size(124, 19);
-            label1.TabIndex = 1;
-            label1.Text = "Primer nombre";
-            // 
             // label2
             // 
             label2.AutoSize = true;
@@ -85,78 +55,12 @@
             label2.TabIndex = 2;
             label2.Text = "Factura";
             // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.ForeColor = Color.White;
-            label3.Location = new Point(267, 84);
-            label3.Name = "label3";
-            label3.Size = new Size(145, 19);
-            label3.TabIndex = 5;
-            label3.Text = "Segundo nombre";
-            // 
-            // TxtSegundoNombre
-            // 
-            TxtSegundoNombre.BackColor = Color.FromArgb(22, 58, 55);
-            TxtSegundoNombre.BorderStyle = BorderStyle.None;
-            TxtSegundoNombre.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
-            TxtSegundoNombre.Location = new Point(267, 121);
-            TxtSegundoNombre.Name = "TxtSegundoNombre";
-            TxtSegundoNombre.Size = new Size(140, 20);
-            TxtSegundoNombre.TabIndex = 4;
-            TxtSegundoNombre.Text = "_________________________________________________________________________";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.ForeColor = Color.White;
-            label4.Location = new Point(647, 84);
-            label4.Name = "label4";
-            label4.Size = new Size(149, 19);
-            label4.TabIndex = 9;
-            label4.Text = "Segundo Apellido";
-            // 
-            // TxtSegundoApellido
-            // 
-            TxtSegundoApellido.BackColor = Color.FromArgb(22, 58, 55);
-            TxtSegundoApellido.BorderStyle = BorderStyle.None;
-            TxtSegundoApellido.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
-            TxtSegundoApellido.Location = new Point(647, 121);
-            TxtSegundoApellido.Name = "TxtSegundoApellido";
-            TxtSegundoApellido.Size = new Size(140, 20);
-            TxtSegundoApellido.TabIndex = 8;
-            TxtSegundoApellido.Text = "_________________________________________________________________________";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.ForeColor = Color.White;
-            label5.Location = new Point(456, 84);
-            label5.Name = "label5";
-            label5.Size = new Size(128, 19);
-            label5.TabIndex = 7;
-            label5.Text = "Primer Apellido";
-            // 
-            // TxtPrimerApellido
-            // 
-            TxtPrimerApellido.BackColor = Color.FromArgb(22, 58, 55);
-            TxtPrimerApellido.BorderStyle = BorderStyle.None;
-            TxtPrimerApellido.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
-            TxtPrimerApellido.Location = new Point(456, 121);
-            TxtPrimerApellido.Name = "TxtPrimerApellido";
-            TxtPrimerApellido.Size = new Size(140, 20);
-            TxtPrimerApellido.TabIndex = 6;
-            TxtPrimerApellido.Text = "_________________________________________________________________________";
-            // 
             // label6
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label6.ForeColor = Color.White;
-            label6.Location = new Point(456, 188);
+            label6.Location = new Point(420, 98);
             label6.Name = "label6";
             label6.Size = new Size(97, 19);
             label6.TabIndex = 13;
@@ -167,7 +71,7 @@
             TxtMontoTotal.BackColor = Color.FromArgb(22, 58, 55);
             TxtMontoTotal.BorderStyle = BorderStyle.None;
             TxtMontoTotal.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
-            TxtMontoTotal.Location = new Point(456, 225);
+            TxtMontoTotal.Location = new Point(420, 135);
             TxtMontoTotal.Name = "TxtMontoTotal";
             TxtMontoTotal.Size = new Size(140, 20);
             TxtMontoTotal.TabIndex = 12;
@@ -178,7 +82,7 @@
             label7.AutoSize = true;
             label7.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label7.ForeColor = Color.White;
-            label7.Location = new Point(265, 188);
+            label7.Location = new Point(229, 98);
             label7.Name = "label7";
             label7.Size = new Size(160, 19);
             label7.TabIndex = 11;
@@ -189,7 +93,7 @@
             TxtNumeroFactura.BackColor = Color.FromArgb(22, 58, 55);
             TxtNumeroFactura.BorderStyle = BorderStyle.None;
             TxtNumeroFactura.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
-            TxtNumeroFactura.Location = new Point(265, 225);
+            TxtNumeroFactura.Location = new Point(229, 135);
             TxtNumeroFactura.Name = "TxtNumeroFactura";
             TxtNumeroFactura.Size = new Size(140, 20);
             TxtNumeroFactura.TabIndex = 10;
@@ -199,7 +103,7 @@
             // 
             CboTipoDePago.FormattingEnabled = true;
             CboTipoDePago.Items.AddRange(new object[] { "Credito", "Transferencia", "Tarjeta de Credito", "Efectivo" });
-            CboTipoDePago.Location = new Point(76, 222);
+            CboTipoDePago.Location = new Point(40, 132);
             CboTipoDePago.Name = "CboTipoDePago";
             CboTipoDePago.Size = new Size(121, 23);
             CboTipoDePago.TabIndex = 14;
@@ -209,7 +113,7 @@
             label8.AutoSize = true;
             label8.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label8.ForeColor = Color.White;
-            label8.Location = new Point(73, 179);
+            label8.Location = new Point(37, 89);
             label8.Name = "label8";
             label8.Size = new Size(111, 19);
             label8.TabIndex = 15;
@@ -223,9 +127,9 @@
             BtnPreliminar.FlatStyle = FlatStyle.Flat;
             BtnPreliminar.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
             BtnPreliminar.ForeColor = Color.Black;
-            BtnPreliminar.Location = new Point(647, 188);
+            BtnPreliminar.Location = new Point(585, 124);
             BtnPreliminar.Name = "BtnPreliminar";
-            BtnPreliminar.Size = new Size(132, 31);
+            BtnPreliminar.Size = new Size(108, 31);
             BtnPreliminar.TabIndex = 17;
             BtnPreliminar.Text = "Preliminar";
             BtnPreliminar.TextAlign = ContentAlignment.BottomCenter;
@@ -275,9 +179,9 @@
             BtnImprimir.FlatStyle = FlatStyle.Flat;
             BtnImprimir.Font = new Font("Century Gothic", 12F, FontStyle.Bold);
             BtnImprimir.ForeColor = Color.Black;
-            BtnImprimir.Location = new Point(647, 241);
+            BtnImprimir.Location = new Point(699, 124);
             BtnImprimir.Name = "BtnImprimir";
-            BtnImprimir.Size = new Size(132, 31);
+            BtnImprimir.Size = new Size(117, 31);
             BtnImprimir.TabIndex = 102;
             BtnImprimir.Text = "Imprimir";
             BtnImprimir.TextAlign = ContentAlignment.BottomCenter;
@@ -298,37 +202,19 @@
             Controls.Add(TxtMontoTotal);
             Controls.Add(label7);
             Controls.Add(TxtNumeroFactura);
-            Controls.Add(label4);
-            Controls.Add(TxtSegundoApellido);
-            Controls.Add(label5);
-            Controls.Add(TxtPrimerApellido);
-            Controls.Add(label3);
-            Controls.Add(TxtSegundoNombre);
             Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(TxtPrimerNombre);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Factura";
             Padding = new Padding(5);
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Factura";
-            Load += Factura_Load_1;
             ((System.ComponentModel.ISupportInitialize)DtgFacturas).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private TextBox TxtPrimerNombre;
-        private Label label1;
         private Label label2;
-        private Label label3;
-        private TextBox TxtSegundoNombre;
-        private Label label4;
-        private TextBox TxtSegundoApellido;
-        private Label label5;
-        private TextBox TxtPrimerApellido;
         private Label label6;
         private TextBox TxtMontoTotal;
         private Label label7;
